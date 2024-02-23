eccee170283e2d2b851f55a6efbd8de4349e8942
sebchm <102329241+sebchm@users.noreply.github.com>
2024-02-23 11:30:45 +0100

    Update 1.run_mapping.sh

--- 2.trim_map_markDup_read/1.run_mapping.sh
+++ 2.trim_map_markDup_read/1.run_mapping.sh
@@ -3,9 +3,9 @@ set -e
 set -u
 set -o pipefail
 
-SAMPLE_LIST=~/sex_ratio_EE/data/sample_list.txt
-FASTQ_DIR=/media/nas/pool_GWAS
+SAMPLE_LIST=(...)/sample_list.txt
+FASTQ_DIR=(...)
 for IND in $(cat ${SAMPLE_LIST})
     do
-    source ~/sex_ratio_EE/bin/2.trim_map_markDup_reads/rr_trim_map.sh ${IND} *${IND}*R1*.fastq.gz *${IND}*R2*.fastq.gz
+    source (...)/rr_trim_map.sh ${IND} *${IND}*R1*.fastq.gz *${IND}*R2*.fastq.gz
     done
