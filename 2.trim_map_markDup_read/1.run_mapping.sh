ea8147dd666c047b8444216aae89048a22fe3be7
sebchm <102329241+sebchm@users.noreply.github.com>
2024-02-24 10:58:28 +0100

    Update 1.run_mapping.sh

--- 2.trim_map_markDup_read/1.run_mapping.sh
+++ 2.trim_map_markDup_read/1.run_mapping.sh
@@ -7,5 +7,5 @@ SAMPLE_LIST=(...)/sample_list.txt
 FASTQ_DIR=(...)
 for IND in $(cat ${SAMPLE_LIST})
     do
-    source rr_trim_map.sh ${IND} VA-3199-${IND}_*R1_001.fastq.gz VA-3199-${IND}_*R1_001.fastq.gz
+    source rr_trim_map.sh ${IND} VA-3199-${IND}_*R1_001.fastq.gz VA-3199-${IND}_*R2_001.fastq.gz
     done
