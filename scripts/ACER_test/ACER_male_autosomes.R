44b26908fd7f8ac699cb4c487b9642982bfa98d6
Sebastian Chmielewski <102329241+sebchm@users.noreply.github.com>
2024-07-10 12:49:37 +0200

    Create ACER_male_autosomes.R

---
+++ scripts/ACER_test/ACER_male_autosomes.R
@@ -0,0 +1,44 @@
+
+### run adapted CMH-test using ACER package
+# author: Sebastian Chmielewski, Evolutionary Biology Group, AMU; sebchm_a_amu.edu.pl
+# male-biased lines (diff in the Ne compared to the female-biased lines)
+
+suppressWarnings(library(ACER))
+suppressWarnings(library(poolSeq))
+
+args = commandArgs(trailingOnly=TRUE)
+infile=args[1]
+
+mySync <- read.sync(infile, gen=c(rep(1,4), rep(28,4)), repl=rep(c(1,2,3,4),2),polarization = "minor")
+
+
+# allele frequency matrix
+af <- af(mySync, repl = rep(c(1,2,3,4),2), gen = c(rep(1,4), rep(28,4)))
+
+# coverage matrix
+cov <- coverage(mySync, repl = rep(c(1,2,3,4),2), gen = c(rep(1,4), rep(28,4)))
+
+# load effective population size
+Ne <- read.table("~/sex_ratio_EE/results/10.estimate_Ne/Ne_est_all_lines.txt", h = T)
+
+pval_ACER <- adapted.cmh.test(freq = af, 
+                           coverage = cov, 
+                           gen = c(1,28),
+                           repl = c(1,2,3,4), 
+                           Ne=c(Ne[5,2], Ne[6,2], Ne[7,2], Ne[8,2]),
+                           poolSize = rep(200, 8),
+                           IntGen = FALSE, 
+                           order = 0, 
+                           mincov = 53, 
+                           RetVal = 0, 
+                           correct = TRUE)
+
+SNP_pvalue <- data.frame(SNP = row.names(af), 
+                         pvalue = pval_ACER)
+
+options(max.print=1000000, width = 10000)
+
+
+if (nrow(SNP_pvalue) > 0) {
+  print(SNP_pvalue, row.names=FALSE)
+}
