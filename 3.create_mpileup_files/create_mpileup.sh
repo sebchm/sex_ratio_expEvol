d99e6b7224b4e0e169e4a689a9baa11417359057
Sebastian Chmielewski <102329241+sebchm@users.noreply.github.com>
2024-09-18 18:09:03 +0200

    Update create_mpileup.sh

--- 3.create_mpileup_files/create_mpileup.sh
+++ 3.create_mpileup_files/create_mpileup.sh
@@ -3,17 +3,17 @@ set -e
 set -u
 set -o pipefail
 
-# create bam list for mpileup
-SAMPLE_LIST=(...)/sex_ratio_EE/data/sample_list.txt
-BAM_LIST=(...)/3.create_mpileup_files/bam_list_for_mpileup.txt
+mkdir ~/sex_ratio_EE/results/3.create_mpileup_files
+    # 4.1 create bam list for mpileup
+    BAM_LIST=~/sex_ratio_EE/results/3.create_mpileup_files/bam_list_for_mpileup.txt
 
-# extract names of fastq files:
-for SAMPLE in $(cat ${SAMPLE_LIST}); do
-    echo "(...)/2.trim_map_markDup_reads/${SAMPLE}/${SAMPLE}.dupmarked.AmbigRm.bam" 
-done > ${BAM_LIST}
+    # 4.2 extract names of fastq files:
+    for SAMPLE in $(cat ${SAMPLE_LIST}); do
+        echo "~/sex_ratio_EE/results/2.trim_map_markDup_reads/${SAMPLE}/${SAMPLE}.dupmarked.AmbigRm.bam" 
+    done > ${BAM_LIST}
 
-# create mpileup file for all samples
-REF=(...)/sex_ratio_EE/data/Rhrob_anchored.fa
+    # 4.3 create mpileup file for all samples
+    REF=~/sex_ratio_EE/data/Rhrob_anchored.fa # reference genome
 
-samtools mpileup -b ${BAM_LIST} -B -Q 0 -f ${REF} -o (...)/EE_SR.mpileup
-gzip (...)/EE_SR.mpileup
+    samtools mpileup -b ${BAM_LIST} -B -Q 0 -f ${REF} -o ~/sex_ratio_EE/results/3.create_mpileup_files/EE_SR.mpileup # gzip later
+
