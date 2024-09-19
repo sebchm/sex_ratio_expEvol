4f308fd786f6546dc62e27defe0f9e9840237bfc
Sebastian Chmielewski <102329241+sebchm@users.noreply.github.com>
2024-09-19 09:56:42 +0200

    Update 1.subsample_coverage_mpileup_filter_positions.sh

--- 8.subsampleCoverage_mpileup/1.subsample_coverage_mpileup_filter_positions.sh
+++ 8.subsampleCoverage_mpileup/1.subsample_coverage_mpileup_filter_positions.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
 ##################################################
 ### 8) SUBSAMPLE MPILEUP TO A UNIFORM COVERAGE ###
 ##################################################
